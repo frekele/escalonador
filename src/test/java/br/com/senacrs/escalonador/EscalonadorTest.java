e29618786601336f6c9bace13c2243e25e4fb592
Leandro Kersting de Freitas <leandro@frekele.org>
2019-09-26 17:42:11 -0300

    .

---
+++ src/test/java/br/com/senacrs/escalonador/EscalonadorTest.java
@@ -0,0 +1,17 @@
+package br.com.senacrs.escalonador;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class EscalonadorTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
---
+++ src/test/java/br/com/senacrs/escalonador/ProcessoTest.java
@@ -0,0 +1,14 @@
+package br.com.senacrs.escalonador;
+
+import static org.junit.Assert.*;
+
+public class ProcessoTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+}
